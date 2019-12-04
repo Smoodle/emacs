061f2e1278ade357bc6683530c14234772738829
Miguel Morgado <miguel.morgado@dev2grow.com>
2019-12-04 15:30:08 +0000

    Initial commit

---
+++ elpa/treemacs-icons-dired-20190719.815/treemacs-icons-dired-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-icons-dired" "20190719.815" "Treemacs icons for dired" '((treemacs "0.0") (emacs "25.2") (cl-lib "0.5")) :commit "80a24e34757566e31e1d3e5f6cc6b4378dfbde4d" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
